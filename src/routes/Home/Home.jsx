87ed676cbb110253d776464bfb1de738b09016e3
lucas santos <lucas2019santos25@gmail.com>
2024-07-02 16:24:26 -0300

    Atualizacao btn 'Fale comigo' e 'Downloand cv'

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,8 +1,32 @@
 import "./Home.css";
-import '../../Responsivo.css'
+import "../../Responsivo.css";
 import minhaImagem from "../../assets/MinhaImagem/minh_imagem.jpg";
+import curriculo from "../../assets/curriculo/LUCAS-SANTOS-NASCIMENTO (1) (1) (1).pdf";
+import { useState } from "react";
 
 const Home = () => {
+  const [carregando, setCarregando] = useState(false);
+  const [redirecionando, setRedirecionando] = useState(false);
+
+  const mudar = () => {
+    setCarregando(true);
+    setTimeout(() => {
+      setCarregando(false);
+    }, 1000);
+  };
+
+  const redirecionar = () => {
+    setRedirecionando(true);
+    setTimeout(() => {
+      setRedirecionando(false);
+      window.open(
+        "https://api.whatsapp.com/send?phone=5519987340162",
+        "_blank"
+      );
+    }, 2000);
+  };
+
+  console.log(carregando);
   return (
     <div className="home-container">
       <div className="home-conteudo">
@@ -16,14 +40,13 @@ const Home = () => {
           projetos. Confira e entre em contato!
         </p>
         <div className="home-btn">
-          <a
-            className="btn"
-            target="_blank"
-            href="https://api.whatsapp.com/send?phone=5519987340162"
-          >
-            Fale comigo
+          <a onClick={redirecionar} className="btn" target="_blank">
+            {redirecionando === true ? "Redirecionando..." : "Fale comigo"}
           </a>
-          <a className="btn">Download CV</a>
+
+          <a onClick={mudar} className="btn" download href={curriculo}>
+            {carregando === true ? "Carregando..." : "Download CV"}
+          </a>
         </div>
       </div>
     </div>
