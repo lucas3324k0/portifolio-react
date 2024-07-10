3cd82f221e2d88c2568846ca170573017b8207bc
lucas santos <lucas2019santos25@gmail.com>
2024-07-09 21:02:53 -0300

    enviado

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { useRouteError } from 'react-router-dom';
+import { useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
   const error = useRouteError();
-  
+
   return (
-    <div className='error-container'
-    style={{
-      width: '100%',
-      height: '100vh',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}
+    <div
+      className="error-container"
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
     >
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
