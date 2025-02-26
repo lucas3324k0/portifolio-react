36c4af0e667f2892d11b873d8d141cf168ee4683
lucas3324k0_ofc <lucas2019santos25@gmail.com>
2025-02-26 13:15:25 -0300

    Atualização do projeto

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -11,6 +12,7 @@ import GetinTouch from "./components/FaleComigo/GetinTouch.jsx";
 import FormContact from "./components/form/FormContact.jsx";
 import SobreMim from "./routes/SobreMim/SobreMim.jsx";
 import ErrorPage from "./ErrorPage.jsx";
+
 
 const router = createBrowserRouter([
   {
